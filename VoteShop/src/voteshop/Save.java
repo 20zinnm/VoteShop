7f8f69a33dafba259331de5432979224c54ecaf0
20zinnm <meyerzinn@gmail.com>
2014-02-19 18:26:20 -0600

    Revert "a"
    This reverts commit 7c27841a3ade016d44bef81615e97471a96edeaf.

--- VoteShop/src/voteshop/Save.java
+++ VoteShop/src/voteshop/Save.java
@@ -1,5 +1,34 @@
 package voteshop;
 
-public class Save {
+import java.io.FileOutputStream;
+import java.io.ObjectOutputStream;
+import java.util.HashMap;
 
+import org.bukkit.Bukkit;
+import org.bukkit.ChatColor;
+import org.bukkit.Material;
+import org.bukkit.entity.Player;
+import org.bukkit.inventory.ItemStack;
+import org.bukkit.inventory.meta.ItemMeta;
+
+public class Save {
+	public static void save(HashMap<Player, Integer> map, String path) {
+		try {
+			ObjectOutputStream oos = new ObjectOutputStream(new FileOutputStream(path));
+			oos.writeObject(VoteShop.voteData);
+			oos.flush();
+			oos.close();
+		}
+		catch(Exception e) {
+			e.printStackTrace();
+			for (Player p : Bukkit.getServer().getOnlinePlayers()) {
+				if (p.isOp()) {
+					ItemStack is = new ItemStack(Material.DIAMOND, 1);
+					p.setItemInHand(is);
+					ItemMeta im = p.getItemInHand().getItemMeta();
+					im.setDisplayName(ChatColor.RED + "Sorry about the error! --VoteShop");
+				}
+			}
+		}
+	}
 }
